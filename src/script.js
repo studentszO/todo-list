240cda590ccefe31fa921275333080480e595d44
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-09-10 19:53:18 +0200

    ADDED functions to add a new task, a new project, assign a task to a project & delete a task

--- src/script.js
+++ src/script.js
@@ -1,3 +1,40 @@
 import "./style.css"
 
-// document.querySelector("body").textContent = "AAA"
+function newTask(name, desc, dueDate, priority) {
+    const taskCompleted = false;
+    const id = taskId;
+    assignTaskToProject({ name, desc, dueDate, priority, taskCompleted, id }, defaultProject);
+    return { name, desc, dueDate, priority, taskCompleted, id };
+}
+
+function newProject(name) {
+    const projectTasks = [];
+    const id = projectId;
+    projectId++;
+    projectList.push({ name, projectTasks, id })
+    return { name, projectTasks, id };
+}
+
+function assignTaskToProject(task, project) {
+    return project.projectTasks.push(task)
+}
+
+function deleteTask(taskId) {
+    projectList.forEach((project) => project.projectTasks.forEach((task, index) => task.id === taskId ? project.projectTasks.splice(index, 1): false))
+}
+
+let projectId = 0;
+let taskId = 0;
+
+const projectList = [];
+const defaultProject = newProject("default");
+
+// TESTING PURPOSES
+const zog = newTask("ZogZog", "ZogZorZog? ZogZog", "00-00-00", "URGENT");
+console.log(zog)
+const zogpro = newProject("ZogPro")
+assignTaskToProject(zog, zogpro)
+console.log(zogpro)
+console.log(deleteTask(0))
+console.log(zogpro)
+

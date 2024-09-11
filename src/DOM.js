85bb44663ce2ca896abfc6a948b684e171d248a7
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-09-11 10:21:41 +0200

    ADDED a function to render the main block with a certain project and the project's tasks associated when you click on a project on the navbar

--- src/DOM.js
+++ src/DOM.js
@@ -2,12 +2,18 @@ import { taskFactory } from "./tasks";
 
 export function renderNav() {
 
+    // const onClick = () => {
+    //     addEventListener("click", (e) => {
+    //      if project e.RenderMainProject, etc..
+    //     })
+    // }
+
     const ulOfProjects = document.querySelector(".projects-list");
 
     taskFactory.projectList.forEach((e) => {
         const projectList = document.createElement("li");
         const projectLink = document.createElement("a");
-        projectLink.setAttribute("href", "#")
+        projectLink.setAttribute("href", "#");
         projectList.appendChild(projectLink);
         projectLink.textContent = e.name;
         ulOfProjects.appendChild(projectList);
@@ -21,5 +27,52 @@ export function renderNav() {
             projectList.appendChild(taskList);
         });
     });
+};
+
+export function renderMain() {
     
+    const mainContainer = document.querySelector(".main");
+    const renderDefault = () => {
+
+    };
+
+    const renderProject = (projectId) => {
+        const listOfProjects = taskFactory.projectList;
+        const title = document.createElement("h2");
+        const tasksContainer = document.createElement("div");
+        const projectTasks = listOfProjects[projectId].projectTasks;
+
+        title.textContent = listOfProjects[projectId].name;
+        mainContainer.append(title, tasksContainer);
+
+        projectTasks.forEach((task) => {
+            const taskCard = document.createElement("div");
+            const taskName = document.createElement("h3");
+            const linkOnH3 = document.createElement("a");
+            const taskDesc = document.createElement("p");
+            const taskPriority = document.createElement("div");
+            const taskDueDate = document.createElement("div");
+
+            linkOnH3.textContent = task.name;
+            taskDesc.textContent = task.desc;
+            taskDueDate.textContent = task.dueDate;
+            taskPriority.textContent = task.priority;
+
+            linkOnH3.setAttribute("href", "#")
+
+            taskName.appendChild(linkOnH3);
+            taskCard.append(taskName, taskDesc, taskPriority, taskDueDate)
+            tasksContainer.append(taskCard);
+        })
+
+
+    };
+
+    const renderTask = () => {
+        // const dueDate = document.createElement("div");
+        // const priority = document.createElement("div");
+        // const description = document.createElement("div");
+    };
+
+    return { renderDefault, renderProject, renderTask }
 }

102f4bf67e0af17a2f3a124cf8e03f9f65a6845c
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-09-28 13:36:51 +0200

    Removed the projectList array as it was a duplicate from projectTasks & reworked all functions to work with an array index instead of searching with an ID

--- src/tasks.js
+++ src/tasks.js
@@ -1,20 +1,21 @@
 export const taskFactory = (function () {
 
-    function newTask(name, desc, dueDate, priority, projectId) {
+    function newTask(name, desc, dueDate, priority, categoryIndex, projectIndex) {
         const taskCompleted = false;
         const id = taskId;
         taskId++;
+        // const categoryIndex = categoryList.findIndex((category) => category.id === Number(categoryId));
+        // const projectIndex = categoryList[categoryIndex].categoryProjects.findIndex((project) => project.id === Number(projectId));
         assignTaskToProject({ name, desc, dueDate, priority, taskCompleted, id }, 
-                            projectList[projectList.findIndex(project => project.id === Number(projectId))] || defaultProject);
+                              categoryList[categoryIndex].categoryProjects[projectIndex] || defaultProject);
         return { name, desc, dueDate, priority, taskCompleted, id };
     }
 
-    function newProject(name, categoryId) {
+    function newProject(name, categoryIndex) {
         const projectTasks = [];
         const id = projectId;
         projectId++;
-        projectList.push({ name, projectTasks, id })
-        assignProjectToCategory({ name, projectTasks, id }, categoryList[categoryList.findIndex(category => category.id === Number(categoryId))] || defaultCategory);
+        assignProjectToCategory({ name, projectTasks, id }, categoryList[categoryIndex] || defaultCategory);
         return { name, projectTasks, id };
     }
 
@@ -34,22 +35,29 @@ export const taskFactory = (function () {
         return category.categoryProjects.push(project);
     }
 
-    function removeTask(taskId) {
-        projectList.forEach((project) => project.projectTasks.forEach((task, index) => task.id === taskId ? project.projectTasks.splice(index, 1): false))
+    function removeTask(categoryIndex, projectIndex, taskIndex) {
+        console.log(categoryIndex, projectIndex, taskIndex)
+        // projectList.forEach((project) => project.projectTasks.forEach((task, index) => task.id === taskId ? project.projectTasks.splice(index, 1): false))
+        categoryList[categoryIndex].categoryProjects[projectIndex].projectTasks.splice(taskIndex, 1)
     }
 
-    function editTask(taskId, taskName, taskDesc, taskDueDate, taskPriority) {
-        projectList.forEach((project) => project.projectTasks.forEach((task, index) => task.id === taskId ? (task.name = taskName) && (task.desc = taskDesc) && (task.dueDate = taskDueDate) && (task.priority = taskPriority) : false))
+    function editTask(taskName, taskDesc, taskDueDate, taskPriority, arrayOfIndexes) {
+        //TODO --------- projectList.forEach((project) => project.projectTasks.forEach((task, index) => task.id === taskId ? (task.name = taskName) && (task.desc = taskDesc) && (task.dueDate = taskDueDate) && (task.priority = taskPriority) : false))
+        const task = categoryList[arrayOfIndexes[0]].categoryProjects[arrayOfIndexes[1]].projectTasks[arrayOfIndexes[2]];
+        task.name = taskName;
+        task.desc = taskDesc;
+        task.dueDate = taskDueDate;
+        task.priority = taskPriority;
     }
 
-    function removeCategory(categoryId) {
-        const index = categoryList.findIndex(category => category.id === Number(categoryId));
-        categoryList.splice(index, 1);
+    function removeCategory(categoryIndex) {
+        // const index = categoryList.findIndex(category => category.id === Number(categoryId));
+        categoryList.splice(categoryIndex, 1);
     }
 
-    function removeProject(projectId) {
-        const index = projectList.findIndex(project => project.id === Number(projectId));
-        projectList.splice(index, 1);
+    function removeProject(categoryIndex, projectIndex) {
+        // const index = projectList.findIndex(project => project.id === Number(projectId));
+        categoryList[categoryIndex].categoryProjects.splice(projectIndex, 1);
     }
 
     let projectId = 0;
@@ -57,9 +65,8 @@ export const taskFactory = (function () {
     let categoryId = 0;
 
     const categoryList = [];
-    const projectList = [];
     const defaultCategory = newCategory("My First Category");
     const defaultProject = newProject("My First project");
 
-    return { newTask, newProject, assignTaskToProject, removeTask, projectList, newCategory, categoryList, editTask, removeCategory, removeProject }
+    return { newTask, newProject, assignTaskToProject, removeTask, newCategory, categoryList, editTask, removeCategory, removeProject }
 })();

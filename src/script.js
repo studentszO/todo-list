dd10465971c6057aaacbfa7a8d1b97dda298fe5b
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-09-29 18:17:57 +0200

    Removed some other lines to make it clean and move the first task exemple into the tasks.js file

--- src/script.js
+++ src/script.js
@@ -1,9 +1,5 @@
 import "./style.css"
-import { taskFactory } from "./tasks.js";
 import { renderMain, renderNav, renderMainLinksContent, setTodaysDateMinOnInput } from "./DOM.js";
-
-const today = new Date().toISOString().split('T')[0];
-const myFirstTask = taskFactory.newTask("My First Task", 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac ullamcorper nostra proin laoreet massa. Porta aenean penatibus varius bibendum accumsan adipiscing cubilia diam hac.', today, "2", 0, 0);
 
 renderNav();
 renderMain().renderProject();

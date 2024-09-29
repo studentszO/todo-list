31c03faaf4ee5ceab47dbcc7c66d0b7db96c8866
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-09-29 18:23:47 +0200

    Added localStorage to store our task in our local storage!

---
+++ src/localStorage.js
@@ -0,0 +1,41 @@
+import { taskFactory } from "./tasks";
+
+export function saveObjectToLocalStorage() {
+    const setCategoriesListObjectToJSON = JSON.stringify(taskFactory.categoryList);
+    const setInboxTasksListObjectToJSON = JSON.stringify(taskFactory.inboxTasks);
+
+    localStorage.setItem("inbox", setInboxTasksListObjectToJSON);
+    localStorage.setItem("categories", setCategoriesListObjectToJSON);
+};
+
+export function loadOBjectFromLocalStorage() {
+    if (storageAvailable("localStorage")) {
+        if (localStorage.length > 0) {
+            const categoriesList = JSON.parse(localStorage.getItem("categories"));
+            const inboxList = JSON.parse(localStorage.getItem("inbox"))
+            return [categoriesList, inboxList]
+          } 
+        else {
+            return false;
+          }
+    }
+};
+
+function storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        e.name === "QuotaExceededError" &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
+  };
--- src/tasks.js
+++ src/tasks.js
@@ -1,3 +1,6 @@
+import { loadOBjectFromLocalStorage } from "./localStorage";
+import { saveObjectToLocalStorage } from "./localStorage";
+
 export const taskFactory = (function () {
 
     function newTask(name, desc, dueDate, priority, categoryIndex, projectIndex) {
@@ -11,32 +14,35 @@ export const taskFactory = (function () {
             assignTaskToProject({ name, desc, dueDate, priority, taskCompleted, id }, 
                               categoryList[categoryIndex].categoryProjects[projectIndex]);
         }
+        saveObjectToLocalStorage();
         return { name, desc, dueDate, priority, taskCompleted, id };
-    }
+    };
 
     function newProject(name, categoryIndex) {
         const projectTasks = [];
         const id = projectId;
         projectId++;
         assignProjectToCategory({ name, projectTasks, id }, categoryList[categoryIndex] || defaultCategory);
+        saveObjectToLocalStorage();
         return { name, projectTasks, id };
-    }
+    };
 
     function newCategory(name) {
         const categoryProjects = [];
         const id = categoryId;
         categoryId++;
         categoryList.push({ name, categoryProjects, id });
+        saveObjectToLocalStorage();
         return { name, categoryProjects, id }
-    }
+    };
 
     function assignTaskToProject(task, project) {
         return project.projectTasks.push(task);
-    }
+    };
 
     function assignProjectToCategory(project, category) {
         return category.categoryProjects.push(project);
-    }
+    };
 
     function removeTask(arrayOfIndexes) {
         console.log(arrayOfIndexes)
@@ -44,7 +50,8 @@ export const taskFactory = (function () {
             inboxTasks.splice(arrayOfIndexes[0], 1)
         else
             categoryList[arrayOfIndexes[0]].categoryProjects[arrayOfIndexes[1]].projectTasks.splice(arrayOfIndexes[2], 1)
-    }
+        saveObjectToLocalStorage();
+    };
 
     function editTask(taskName, taskDesc, taskDueDate, taskPriority, arrayOfIndexes) {
         console.log(arrayOfIndexes)
@@ -58,24 +65,34 @@ export const taskFactory = (function () {
         task.desc = taskDesc;
         task.dueDate = taskDueDate;
         task.priority = taskPriority;
-    }
+        saveObjectToLocalStorage();
+    };
 
     function removeCategory(categoryIndex) {
         categoryList.splice(categoryIndex, 1);
-    }
+        saveObjectToLocalStorage();
+    };
 
     function removeProject(categoryIndex, projectIndex) {
         categoryList[categoryIndex].categoryProjects.splice(projectIndex, 1);
-    }
+        saveObjectToLocalStorage();
+    };
 
     let projectId = 0;
     let taskId = 0;
     let categoryId = 0;
 
-    const inboxTasks = [];
-    const categoryList = [];
-    const defaultCategory = newCategory("My First Category");
-    const defaultProject = newProject("My First project");
+    const loadStorage = loadOBjectFromLocalStorage();
+
+    const inboxTasks = loadStorage === false ? [] : loadStorage[1];
+    const categoryList = loadStorage === false ? [] : loadStorage[0];
+
+    if (loadStorage === false) {
+        const today = new Date().toISOString().split('T')[0];
+        const myFirstTask = taskFactory.newTask("My First Task", 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac ullamcorper nostra proin laoreet massa. Porta aenean penatibus varius bibendum accumsan adipiscing cubilia diam hac.', today, "2", 0, 0);
+        const defaultCategory = newCategory("My First Category");
+        const defaultProject = newProject("My First project");
+    }
 
     return { newTask, newProject, assignTaskToProject, removeTask, newCategory, categoryList, inboxTasks, editTask, removeCategory, removeProject }
 })();

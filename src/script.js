deca93303465a385e5cb1a2cc29ea2fd8e795967
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-09-29 14:03:13 +0200

    Deleted useless lines of code used for testing purposes and added a first task to show on pageload

--- src/script.js
+++ src/script.js
@@ -1,22 +1,11 @@
 import "./style.css"
 import { taskFactory } from "./tasks.js";
-import { renderMain, renderNav, renderMainLinksContent } from "./DOM.js";
+import { renderMain, renderNav, renderMainLinksContent, setTodaysDateMinOnInput } from "./DOM.js";
 
-// TESTING PURPOSES
-// const zog = taskFactory.newTask("ZogZog", "ZogZorZog? ZogZog", "00-00-00", "4", 1, 0);
-const zog2 = taskFactory.newTask("ZogZog2", "ZogZorZog? ZogZog", "00-00-00", "2", 0, 0);
-const xxx = taskFactory.newTask("Bahamut", "Best bahamut LOL", "00-00-01", "2");
-// const zog3 = taskFactory.newTask("ZogZog3", "ZogZorZog? ZogZog", "00-00-00", "3", 1, 0);
-// const zog4 = taskFactory.newTask("ZogZog4", "ZogZorZog? ZogZog", "00-00-00", "1", 0, 0);
-const cat1 = taskFactory.newCategory("HOME");
-const cat2 = taskFactory.newCategory("WORK");
-const zogpro = taskFactory.newProject("ZogPro", 1)
-const zogZogZogZogZog = taskFactory.newProject("ZogPro", 2)
-const zogZogPro = taskFactory.newProject("ZogProPro", 2)
-const zogZogPro11 = taskFactory.newProject("ZogProProHOME", 1)
-// taskFactory.assignTaskToProject(zog, zogpro)
+const today = new Date().toISOString().split('T')[0];
+const myFirstTask = taskFactory.newTask("My First Task", 'Lorem ipsum odor amet, consectetuer adipiscing elit. Hac ullamcorper nostra proin laoreet massa. Porta aenean penatibus varius bibendum accumsan adipiscing cubilia diam hac.', today, "2", 0, 0);
 
-console.log(taskFactory.categoryList)
 renderNav();
 renderMain().renderProject();
 renderMainLinksContent();
+setTodaysDateMinOnInput();
